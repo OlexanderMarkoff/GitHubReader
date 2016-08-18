e78211cb772c7c628cf3e5ed1eda935ad7befebf
OlexanderMarkoff <ddrecass@gmail.com>
2016-08-18 22:53:11 +0300

    First commit Login Activity

---
+++ app/src/main/java/com/example/m1/githubreader/activities/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.m1.githubreader.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.m1.githubreader.R;
+
+/**
+ * Created by M1 on 18.08.2016.
+ */
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
+    initUI();
+    setUI();
+    }
+
+    private void setUI() {}
+
+    private void initUI() {}
+
+}
