bae80b1b86aed7feb0b7f47f20d2525869859187
OlexanderMarkoff <ddrecass@gmail.com>
2016-08-22 16:04:42 +0300

    API implementation the second step

--- app/src/main/java/com/example/m1/githubreader/api/APIHelper.java
+++ app/src/main/java/com/example/m1/githubreader/api/APIHelper.java
@@ -1,9 +1,9 @@
 package com.example.m1.githubreader.api;
 
-import com.rejasupotaro.octodroid.GitHub;
-
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -37,7 +37,45 @@ public class APIHelper {
         mRestService = mRestAdapter.create(APIHelperInterface.class);
     }
 
-    public static void login () {
-        GitHub.client().authorization("username", "password");
+    public static class GlobalDataResponse<T> {
+        public int error;
+        public String message;
+        public int status;
+        public T data;
     }
+
+    private static <T> Callback<GlobalDataResponse<T>> checkRetrofitResponse(
+            final Callback<GlobalDataResponse<T>> incomingCallback) {
+        return new Callback<GlobalDataResponse<T>>() {
+            @Override
+            public void onResponse(Call<GlobalDataResponse<T>> call, retrofit2.Response<GlobalDataResponse<T>> response) {
+                if (response == null || response.body() == null) {
+                    incomingCallback.onFailure(call, new Throwable("SERVER ERROR"));
+                } else if (response.body().data != null && response.body().error == 0
+                        && response.body().status == 1) {
+                    incomingCallback.onResponse(call, response);
+                    android.util.Log.i("Retrofit", "successful");
+                } else {
+                    incomingCallback.onFailure(call, new Throwable(response.body().message));
+                    android.util.Log.i("Retrofit", "failure");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GlobalDataResponse<T>> call, Throwable t) {
+                incomingCallback.onFailure(call, t);
+                android.util.Log.i("Retrofit", "failure");
+            }
+        };
+    }
+
+    /**
+     * @param login
+     * @param password
+     */
+
+    public static void authorization(String login, String password, Callback<GlobalDataResponse<Void>> callback) {
+
+    }
+
 }
